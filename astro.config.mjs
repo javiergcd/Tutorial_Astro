8187c6f0a246b3fb61a55c888273eee90769ab5f
Javier Corrales <jg.corrales9@gmail.com>
2024-05-05 11:46:13 -0400

    Primera isla de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://wondrous-salmiakki-294d9f.netlify.app/"
+  site: "https://wondrous-salmiakki-294d9f.netlify.app/",
+  integrations: [preact()]
 });
